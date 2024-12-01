011d178f831ba14d332981cf7322350917b50451
Israel Antezana Rojas <israelantezana@gmail.com>
2024-12-01 14:24:03 -0400

    Cadena de un numero

--- src/calculadoraCadenas.spec.js
+++ src/calculadoraCadenas.spec.js
@@ -2,8 +2,15 @@ describe("Calculadora de cadenas", () => {
   it("cadena vacia suma 0", () => {
     expect(sumar("")).toEqual(0);
   });
+
+  it("cadena de un numero retorna el mismo numero", () => {
+    expect(sumar("1")).toEqual(1);
+  });
 });
 
 function sumar(cadena) {
-  return 0;
+  if (cadena === "") {
+    return 0;
+  }
+  return Number.parseInt(cadena);
 }
